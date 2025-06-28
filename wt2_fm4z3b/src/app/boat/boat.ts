5d663b31f98e2444fb75bfe66acb1500b9b2a476
pogacsasb <pogacsasb@gmail.com>
2025-06-28 12:23:58 +0200

    wt2

---
+++ wt2_fm4z3b/src/app/boat/boat.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boat',
+  imports: [],
+  templateUrl: './boat.html',
+  styleUrl: './boat.css'
+})
+export class Boat {
+
+}
