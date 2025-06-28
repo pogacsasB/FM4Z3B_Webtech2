d7f41d24ffb6ef358f12688eef83c3590af53094
pogacsasb <pogacsasb@gmail.com>
2025-06-28 13:17:59 +0200

    wt2

--- wt2_fm4z3b/src/app/app.routes.ts
+++ wt2_fm4z3b/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Boat } from './boat/boat';
+import { Boatform } from './boatform/boatform';
 
 export const routes: Routes = [
     {path:"", component: Boat},
+    {path:"boat-entry", component: Boatform}
 ];
